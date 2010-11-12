d699dc2b4c944a94bec3ec20d4273dccfa6bf1d0
Arunjit Singh <arunjitsingh1190@gmail.com>
2010-11-12 17:39:50 +0530

    basic start.. don't really know what to do

---
+++ server.js
@@ -0,0 +1,29 @@
+var net = require('net'),
+  count = 0;
+
+var server = net.createServer(function(stream) {
+  console.log("Stream connected");
+
+  stream.setEncoding('utf8');
+
+  stream.on('connect', function() {
+    stream.
+    stream.write("Hello!\r\n");
+  });
+
+  stream.on('data', function(data) {
+    data = data.replace(/[\r\n]/ig, "");
+    console.log("remote said: " + data);
+    stream.write("you said: " + data + "\r\n");
+  });
+
+  stream.on('end', function() {
+    stream.write("Bye!");
+    stream.end();
+
+    console.log("Stream closed");
+  });
+});
+
+server.listen(9100);
+console.log("TCP listening on port 9100");
